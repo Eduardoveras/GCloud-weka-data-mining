9c313f9757efd94062159d6c1e8454d2c909f0df
Eduardo Veras <eduardo.storm@gmail.com>
2017-07-02 12:09:30 -0400

    First commit

---
+++ src/main/java/MainWindow.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+/**
+ * Created by eduardo on 02/07/17.
+ */
+public class MainWindow extends JFrame {
+    public MainWindow ()
+    {
+        this.setVisible(true);
+        this.setSize(200,200);
+    }
+}
