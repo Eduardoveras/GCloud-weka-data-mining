ee81e7732c1c47330ee1b8bb2a3bee7edd2a9128
Eduardo Veras <eduardo.storm@gmail.com>
2017-07-20 19:26:32 -0400

    finally finished

--- src/main/java/MainPanel.java
+++ src/main/java/MainPanel.java
@@ -1,5 +1,6 @@
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.functions.SMO;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils;
 import weka.filters.Filter;
@@ -12,6 +13,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.Random;
 
 public class MainPanel extends JPanel {
 
@@ -48,19 +50,47 @@ public class MainPanel extends JPanel {
                 }
             }
         });
-        btnPhoto.setBounds(12, 582, 636, 43);
+        btnPhoto.setBounds(241, 395, 320, 43);
         add(btnPhoto);
         
         txtEnterClass = new JTextField();
         txtEnterClass.setToolTipText("Enter Class");
-        txtEnterClass.setBounds(35, 473, 166, 43);
+        txtEnterClass.setBounds(12, 395, 166, 43);
         add(txtEnterClass);
         txtEnterClass.setColumns(10);
         
         JLabel lblClassAtribute = new JLabel("Class Atribute");
         lblClassAtribute.setFont(new Font("DejaVu Sans Mono", Font.PLAIN, 16));
-        lblClassAtribute.setBounds(35, 433, 218, 28);
+        lblClassAtribute.setBounds(12, 355, 218, 28);
         add(lblClassAtribute);
+        
+        JButton btnNewButton = new JButton("Process from local dataset");
+        btnNewButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        		try {
+        			processLocalDataset();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+        	}
+        });
+        btnNewButton.setBounds(241, 451, 320, 49);
+        add(btnNewButton);
+        
+        JButton btnNewButton_1 = new JButton("Procesar con 10-fold");
+        btnNewButton_1.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		try {
+					processDataset();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+        	}
+        });
+        btnNewButton_1.setBounds(241, 512, 320, 43);
+        add(btnNewButton_1);
 
     }
 
@@ -77,12 +107,12 @@ public class MainPanel extends JPanel {
 
         Instances data = getFilteredDataSet("train-data/letras.arff");
         Instances testData = getFilteredDataSet("train-data/test.arff");
-
-        NaiveBayes nb = new NaiveBayes();
-        nb.buildClassifier(data);
+        
+        SMO svm = new SMO();
+		svm.buildClassifier(data);
 
         Evaluation eval = new Evaluation(data);
-        eval.evaluateModel(nb, testData);
+        eval.evaluateModel(svm, testData);
 
         //eval.crossValidateModel(nb, data, 10, new Random(1));
 
@@ -92,6 +122,69 @@ public class MainPanel extends JPanel {
         System.out.println(eval.toClassDetailsString("Details"));
         txtrContent.append(eval.toSummaryString());
         System.out.println(eval.toSummaryString());
+
+    }
+    
+    
+    public static void processLocalDataset() throws Exception {
+
+        Instances data = getFilteredDataSet("train-data/letras.arff");
+        Instances testData = getFilteredDataSet("train-data/test.arff");
+
+        
+        SMO svm = new SMO();
+		svm.buildClassifier(data);
+
+        Evaluation eval = new Evaluation(data);
+        eval.evaluateModel(svm, testData);
+
+        //eval.crossValidateModel(nb, data, 10, new Random(1));
+
+        txtrContent.append(eval.toMatrixString("Matriz de confucion"));
+        System.out.println(eval.toMatrixString("Matriz de confucion"));
+        txtrContent.append(eval.toClassDetailsString("Details"));
+        System.out.println(eval.toClassDetailsString("Details"));
+        txtrContent.append(eval.toSummaryString());
+        System.out.println(eval.toSummaryString());
+
+    }
+    
+    
+    
+    public static void processDataset() throws Exception {
+
+        Instances data = getFilteredDataSet("train-data/letras.arff");
+        Evaluation eval = new Evaluation(data);
+
+
+        /*NaiveBayes nb = new NaiveBayes();
+        nb.buildClassifier(data);
+        
+
+        eval.crossValidateModel(nb, data, 10, new Random(1));
+        
+
+        txtrContent.append(eval.toMatrixString("Matriz de confucion"));
+        System.out.println(eval.toMatrixString("Matriz de confucion"));
+        txtrContent.append(eval.toClassDetailsString("Details"));
+        System.out.println(eval.toClassDetailsString("Details"));
+        txtrContent.append(eval.toSummaryString());
+        System.out.println(eval.toSummaryString());*/
+        
+        
+        
+		SMO svm = new SMO();
+		svm.buildClassifier(data);
+        eval.crossValidateModel(svm, data, 10, new Random(1));
+        
+        
+        txtrContent.append(eval.toMatrixString("Matriz de confucion"));
+        System.out.println(eval.toMatrixString("Matriz de confucion"));
+        txtrContent.append(eval.toClassDetailsString("Details"));
+        System.out.println(eval.toClassDetailsString("Details"));
+        txtrContent.append(eval.toSummaryString());
+        System.out.println(eval.toSummaryString());
+        
 
     }
 
