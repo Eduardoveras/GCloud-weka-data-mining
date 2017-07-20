d4c28d7d96045ed97278122e4be79223fa644287
Eduardo Veras <eduardo.storm@gmail.com>
2017-07-19 22:02:50 -0400

    yolo again

--- src/main/java/MainPanel.java
+++ src/main/java/MainPanel.java
@@ -18,6 +18,7 @@ import weka.filters.unsupervised.instance.imagefilter.PHOGFilter;
 
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.Random;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextArea;
@@ -40,7 +41,7 @@ public class MainPanel extends JPanel{
 		btnTraindata.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
-					trainModel();
+				    System.out.println();
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -65,7 +66,12 @@ public class MainPanel extends JPanel{
 				if (result == JFileChooser.APPROVE_OPTION) {
 				    File selectedFile = fileChooser.getSelectedFile();
 				    System.out.println("Selected file: " + selectedFile.getAbsolutePath());
-				}
+                    try {
+                        classifySingleInstance(selectedFile.getAbsolutePath());
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
+                    }
+                }
 			}
 		});
 		btnPhoto.setBounds(236, 546, 117, 25);
@@ -74,43 +80,29 @@ public class MainPanel extends JPanel{
 
 	}
 	
+
 	
-	public static void trainModel() throws Exception{
-		
-        ConverterUtils.DataSource source = new ConverterUtils.DataSource("train-data/menos_letras.arff");
-        Instances data = source.getDataSet();
-
-        if (data.classIndex() == -1)
-            data.setClassIndex(data.numAttributes() - 1);
+	public static void classifySingleInstance(String path) throws Exception{
 
 
-        String[] options = new String[2];
-        options[0] = "-D"; options[1] = "train-data";
-        PHOGFilter filter = new PHOGFilter();
-        //ColorLayoutFilter filter = new ColorLayoutFilter();
-        filter.setInputFormat(data);
-        filter.setOptions(options);
+	    /*
+        String filename= "train-data/menos_letras.arff";
+        FileWriter fwriter = new FileWriter(filename,true); //true will append the new instance
+        fwriter.write(path+",?\n");//appends the string to the file
+        fwriter.close();*/
 
-        Instances trainingSubset = Filter.useFilter(data, filter);
-
-        String[] rem_options = new String[2];
-        rem_options[0] = "-R";                                    // "range"
-        rem_options[1] = "1";                                     // first attribute
-        Remove remove = new Remove();                         // new instance of filter
-        remove.setOptions(rem_options);                           // set options
-        remove.setInputFormat(trainingSubset);                          // inform filter about dataset **AFTER** setting options
-        dataSetFinal = Filter.useFilter(trainingSubset, remove);   // apply filter
-
+        Instances data = getFilteredDataSet("train-data/letras.arff");
+        Instances testData = getFilteredDataSet("train-data/test.arff");
 
         NaiveBayes nb = new NaiveBayes();
-        nb.buildClassifier(dataSetFinal);
-        txtrContent.append(nb.getCapabilities().toString());
-        System.out.println(nb.getCapabilities().toString());
+        nb.buildClassifier(data);
 
-        Evaluation eval = new Evaluation(dataSetFinal);
-        eval.crossValidateModel(nb, dataSetFinal, 10, new Random(1));
-        
-        
+        Evaluation eval = new Evaluation(data);
+        eval.evaluateModel(nb, testData);
+
+        //eval.crossValidateModel(nb, data, 10, new Random(1));
+
+
         txtrContent.append(eval.toMatrixString("Matriz de confucion"));
         System.out.println(eval.toMatrixString("Matriz de confucion"));
         txtrContent.append(eval.toClassDetailsString("Details"));
@@ -119,40 +111,34 @@ public class MainPanel extends JPanel{
         System.out.println(eval.toSummaryString());
 		
 	}
-	
-	public static void classifySingleInstance(String path) throws Exception{
-		
-
-		 Attribute filename = new Attribute("filename");
-         Attribute the_class = new Attribute("class");
 
 
-         // Create instances for each pollutant with attribute values latitude, longitude and pollutant itself
-         DenseInstance inst_co = new DenseInstance(4);
 
-         // Set instance's values for the attributes "latitude", "longitude", and "pollutant concentration"
-         inst_co.setValue(filename, path);
-         inst_co.setMissing(the_class);
-         inst_co.setDataset(dataSetFinal);
-         
-         
-         NaiveBayes nb = new NaiveBayes();
-         nb.buildClassifier(dataSetFinal);
-         txtrContent.append(nb.getCapabilities().toString());
-         System.out.println(nb.getCapabilities().toString());
+	public static Instances getFilteredDataSet(String path) throws Exception
+    {
 
-         Evaluation eval = new Evaluation(dataSetFinal);
-         eval.crossValidateModel(nb, dataSetFinal, 10, new Random(1));
-         
-         
-         txtrContent.append(eval.toMatrixString("Matriz de confucion"));
-         System.out.println(eval.toMatrixString("Matriz de confucion"));
-         txtrContent.append(eval.toClassDetailsString("Details"));
-         System.out.println(eval.toClassDetailsString("Details"));
-         txtrContent.append(eval.toSummaryString());
-         System.out.println(eval.toSummaryString());	
-		
-	}
+        ConverterUtils.DataSource source = new ConverterUtils.DataSource(path);
+        Instances data = source.getDataSet();
+        if (data.classIndex() == -1)
+            data.setClassIndex(data.numAttributes() - 1);
+        String[] options = new String[2];
+        options[0] = "-D"; options[1] = "train-data";
+        PHOGFilter filter = new PHOGFilter();
+        filter.setInputFormat(data);
+        filter.setOptions(options);
+
+        Instances trainingSubset = Filter.useFilter(data, filter);
+
+        String[] rem_options = new String[2];
+        rem_options[0] = "-R";
+        rem_options[1] = "1";
+        Remove remove = new Remove();
+        remove.setOptions(rem_options);
+        remove.setInputFormat(trainingSubset);
+        return Filter.useFilter(trainingSubset, remove);
+
+
+    }
 	
 	
 }
